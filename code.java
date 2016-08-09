23e21cf57516825dc476d663507888a7b0958fae
Tianhe Gong (RD-CN-INTRN) <tianhe_gong@tw.trendnet.org>
2016-08-09 18:23:51 +0800

    finished

--- code.java
+++ code.java
@@ -1,16 +1,17 @@
 private boolean find(TreeNode treeNode, int element) {
+	// judge
 	if (treeNode == null)
 		return false;
 	if (element == treeNode.getData())
 		return true;
 	if (element < treeNode.getData()) {
-		return find(treeNode.getLeftChild(), element);
+		return find(treeNode.getLeftChild(), element); /* find left child */
 	} else {
 		return find(treeNode.getRightChild(), element);
 	}
 }
+// This is find function
 public boolean find(element) {
 	find(rootNode, element);
 }
-// the number of tree nodes is n
 // asymptotic time complexity: O(log(n))
